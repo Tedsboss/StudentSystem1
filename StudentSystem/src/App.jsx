721262791d87cdb470132896b746fe3c26855180
MARLON_LANDER ORTEGA <teddyboss0918@gmail.com>
2023-11-28 10:42:50 +0800

    first commit

---
+++ StudentSystem/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import ButtonAppBar from './components/ButtonAppBar'
+import Student from './components/Student'
+function App() {
+
+  return (
+    <>
+    <ButtonAppBar/>
+    <Student/>
+     
+      
+    </>
+  )
+}
+
+export default App
